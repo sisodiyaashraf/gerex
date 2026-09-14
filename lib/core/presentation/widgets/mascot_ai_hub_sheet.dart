3535b3924c2150ff8ac7607bd7b17000ed038803
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-14 10:17:21 +0530

    feat: add MascotAiHubBottomSheet widget for quick AI features and navigation

--- lib/core/presentation/widgets/mascot_ai_hub_sheet.dart
+++ lib/core/presentation/widgets/mascot_ai_hub_sheet.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import '../providers/mascot_controller.dart';
 
