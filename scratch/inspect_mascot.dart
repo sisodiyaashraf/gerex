9e1745c2f40511777f4003bbff13ed614580e162
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-15 09:55:00 +0530

    chore: add script to inspect robot mascot asset files

---
+++ scratch/inspect_mascot.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+  final files = [
+    'assets/images/robot_mascot/gerex_robot_idle.png',
+    'assets/images/robot_mascot/gerex_robot_smiling.png',
+    'assets/images/robot_mascot/gerex_robot_walking.png',
+    'assets/images/robot_mascot/gerex_robot_running.png',
+    'assets/images/robot_mascot/gerex_robot_exercise.png',
+    'assets/images/robot_mascot/gerex_robot_sweating.png',
+    'assets/images/robot_mascot/gerex_robot_sweating_and_tired.png',
+    'assets/images/robot_mascot/gerex_robot_tired.png',
+  ];
+
+  for (final path in files) {
+    final file = File(path);
+    if (file.existsSync()) {
+      print('$path: ${file.lengthSync()} bytes');
+    } else {
+      print('$path: MISSING');
+    }
+  }
+}
