30cffd5da7c1aa405ff7e033e9738baf65825936
sisodiyaashraf <ashrafsisodiya478@gmail.com>
2026-09-15 09:55:27 +0530

    feat: add script to analyze PNG asset file sizes

---
+++ scratch/analyze_png.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  final files = [
+    'assets/images/robot_mascot/gerex_robot_idle.png',
+    'assets/images/robot_mascot/gerex_robot_smiling.png',
+    'assets/images/robot_mascot/gerex_robot_walking.png',
+    'assets/images/robot_mascot/gerex_robot_sweating.png',
+    'assets/images/robot_mascot/gerex_robot_sweating_and_tired.png',
+    'assets/images/robot_mascot/gerex_robot_tired.png',
+  ];
+
+  for (final p in files) {
+    final file = File(p);
+    final bytes = file.readAsBytesSync();
+    print('$p: ${bytes.length} bytes');
+  }
+}
